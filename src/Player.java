e488bfd1d291724f68d07bc4375fe4b92e8a6087
Partast1 <43130360+Partast1@users.noreply.github.com>
2020-02-12 12:58:13 +0100

    thing

--- src/Player.java
+++ src/Player.java
@@ -10,7 +10,7 @@ public class Player {
     Point points = new Point(0);
 
 //    Variables
-    private int rerollCount;
+    private int rerollCount = 0;
     List<Dice> dices = new ArrayList<>();
     List<Integer> rolledEyes = new ArrayList<>();
     List<Dice> reserveDice = new ArrayList<>();
@@ -44,56 +44,49 @@ public class Player {
             num = num + 1;
             rolledEyes.add(num);
         }
-
     }
-
     int diceCount = 1;
 
 //    Show rolled dices
     public void DisplayrolledEyes()
     {
-
-//        System.out.print("Select dices to add to reserve. \n");
         for (Integer num : rolledEyes)
         {
             System.out.print(diceCount + ". " + num + " /");
             diceCount++;
         }
         diceCount = 1;
-
     }
 
     //    Adds wanted dices to the reserve
     public void AddToReserve()
     {
-        boolean addingToReserve = true;
-
-
+        boolean reserveBool = true;
       int unReserve = 9;
         System.out.print("Choose dices to reserve");
-       while (addingToReserve)
-       {
-           Scanner scan = new Scanner(System.in);
-           int diceSelector = scan.nextInt();
-           diceSelector = diceSelector -1;
+        while (reserveBool)
+        {
+            if (rerollCount < 3)
+            {
+                Scanner scan = new Scanner(System.in);
+                int diceSelector = scan.nextInt();
+                diceSelector = diceSelector - 1;
 
-               if (diceSelector <= dices.size())
-               {
-                  Dice dice = dices.get(diceSelector);
-                  reserveDice.add(dice);
-                  int resEye = rolledEyes.get(diceSelector);
-                  dices.remove(diceSelector);
-                  reserveEye.add(resEye);
-                  rolledEyes.remove(diceSelector);
+                if (diceSelector <= dices.size()) {
+                    Dice dice = dices.get(diceSelector);
+                    reserveDice.add(dice);
+                    int resEye = rolledEyes.get(diceSelector);
+                    dices.remove(diceSelector);
+                    reserveEye.add(resEye);
+                    rolledEyes.remove(diceSelector);
 
-                   DisplayrolledEyes();
-
-                   System.out.print("Current reserve " );
-                   for (Integer num: reserveEye)
-                   {
-                       System.out.print(num + " ");
-                   }
-               }
+                    DisplayrolledEyes();
+                    rerollCount++;
+                    System.out.print("Current reserve ");
+                    for (Integer num : reserveEye) {
+                        System.out.print(num + " ");
+                    }
+                }
 //               else if(diceSelector == unReserve)
 ////               {
 ////                   System.out.print("Hej");
@@ -101,104 +94,21 @@ public class Player {
 //////                   reserveDice.clear();
 //////                   reserveEye.clear();
 ////               }
-           else
-               {
-                   System.out.print("Wrong input");
-                   addingToReserve = false;
-               }
-       }
+                else {
+                    System.out.print("Wrong input");
+                    reserveBool = false;
+                }
+            }
+                    else
+                        {
+                            System.out.print("You have used all reserves attempts");
+                            reserveBool = false;
+                        }
+        }
+
+
 
 
 
     }
 }
-
-//import java.util.ArrayList;
-//        import java.util.List;
-//        import java.util.Random;
-//        import java.util.Scanner;
-//
-//public class Player {
-//    //    Class instantiating
-//    DiceFactory diceFac = new DiceFactory();
-//    Combination combination = new Combination();
-//    Point points = new Point(0);
-//
-//    //    Variables
-//    private int rerollCount;
-//    List<Dice> dices = new ArrayList<>();
-//    List<Integer> rolledEyes = new ArrayList<>();
-//    List<Dice> reserveDice = new ArrayList<>();
-//
-//    //    Getters & Setters
-//    public int getRerollCount()
-//    {
-//        return rerollCount;
-//    }
-//    public void setRerollCount(int rerollCount)
-//    {
-//        this.rerollCount = rerollCount;
-//    }
-//
-//    //    Constructor
-//    public Player(){
-//        for (int i = 0; i < 5; i++)
-//        {
-//            dices.add(diceFac.ProduceDice());
-//        }
-//    }
-//
-//    //    Rolls all players dices
-//    public void RollDice()
-//    {
-//        Random rnd = new Random();
-//        for (Dice dice: dices)
-//        {
-//            int num = rnd.nextInt( dice.eyes.size());
-//            rolledEyes.add(dice.eyes.get(num));
-//        }
-//
-//    }
-//
-//    int diceCount = 1;
-//    //    Show rolled dices
-//    public void DisplayrolledEyes()
-//    {
-//
-//        System.out.print("Select dices to add to reserve. \n");
-//        for (Integer num : rolledEyes)
-//        {
-//            System.out.print(diceCount + ". " + num + " /");
-//            diceCount++;
-//        }
-//    }
-//
-//    //    Adds wanted dices to the reserve
-//    public void AddToReserve()
-//    {
-//        boolean addingToReserve = true;
-//        Scanner scan = new Scanner(System.in);
-//        int diceSelector = scan.nextInt();
-////        int unReserve = 9;
-////      HER STARTER VI.
-//        while (addingToReserve)
-//        {
-//            if (diceSelector >= rolledEyes.size())
-//            {
-//                reserveDice.add(dices.);
-//            }
-////              else if(diceSelector = 9)
-//////               {
-//////                   for (Dice dice: reserveDice) {
-//////                       rolledEyes.add(reserveDice.get(dice.));
-//////                   }
-//////               }
-//            else
-//            {
-//                System.out.print("Wrong input");
-//            }
-//        }
-//
-//    }
-//}
-
